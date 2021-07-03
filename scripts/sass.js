b0715e322c76b68e97bbe9a489effc9e7947d693
Sam Davis <sam@sjd.co>
2021-07-03 10:15:40 +1000

    move to dart-sass

--- scripts/sass.js
+++ scripts/sass.js
@@ -1,4 +1,4 @@
-const sass = require("node-sass");
+const sass = require("sass");
 const path = require("path");
 const fs = require("fs");
 const watch = require("node-watch");
@@ -8,26 +8,18 @@ const OUTPUT_FILE = path.join(__dirname, "../dist/treemodal.css");
 const OUTPUT_FILE_MIN = path.join(__dirname, "../dist/treemodal.min.css");
 
 const renderSassToDisk = ({ file, outFile, ...opts }) => {
-    sass.render(
-        {
-            file,
-            outFile,
-            ...opts,
-        },
-        (err, result) => {
+    try {
+        const result = sass.renderSync({ file, outFile, ...opts });
+        fs.writeFile(outFile, result.css, (err) => {
             if (err) {
                 console.error(err);
                 return;
             }
-            fs.writeFile(outFile, result.css, (err) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                console.log(`Successfully wrote ${outFile} to disk`);
-            });
-        }
-    );
+            console.log(`Successfully wrote ${outFile} to disk`);
+        });
+    } catch (error) {
+        console.error(err);
+    }
 };
 
 const go = () => {
